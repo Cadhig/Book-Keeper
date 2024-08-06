f838af2e43798286b2297cbfedc91e6434309968
Cadence K <cadencehiggins@gmail.com>
2024-08-05 21:22:43 -0500

    edited server.js

--- server/server.js
+++ server/server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const { ApolloServer } = require("@apollo/server")
 const path = require('path');
 const db = require('./config/connection');
-const { typeDefs, resolvers } = require('./schemas')
+const { typeDefs } = require('./schemas')
 const routes = require('./routes');
 const { expressMiddleware } = require("@apollo/server/express4")
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+  typeDefs
 });
 const app = express();
 const PORT = process.env.PORT || 3001;
